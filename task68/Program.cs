c308427e5e13439ce8845a67e8d1002191a63673
maks <maks-lapin-1998@.ru>
2023-06-01 21:18:35 +0400

    task68

---
+++ task68/Program.cs
@@ -0,0 +1,21 @@
+﻿int ReadInt(string argument) 
+{
+    Console.Write(argument);
+    int i;
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число!");
+        Console.Write(argument);
+    }
+    return i;
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = ReadInt("Введите M: ");
+int n = ReadInt("Введите N: ");
+System.Console.WriteLine(Akkerman(m, n));
